9a0a95632b8e6caa9bb5ce5f45af621e4945ded9
marb <marb61a@gmail.com>
2015-06-03 08:31:55 +0000

    adding crud functionality

--- controllers/manage.js
+++ controllers/manage.js
@@ -94,4 +94,136 @@ module.exports = function(router){
         });
         
     });
+    
+    // Displays  edit form
+    router.get('/books/edit/:id', function (req, res) {
+         Category.find({},function (err, categories) {
+             Book.findOne({_id:req.params.id},function (err, book) {
+                if (err) {
+                    console.log(err);
+                }
+                var model ={
+                    book: book,
+                    categories: categories
+                };
+                res.render('manage/books/edit', model);
+            });
+        });
+    });
+    
+    // Edit book
+    router.post('/books/edit/:id', function(req, res){
+        var title = req.body.title && req.body.title.trim();
+        var category = req.body.category && req.body.category.trim();
+        var author = req.body.author && req.body.author.trim();
+        var publisher = req.body.publisher && req.body.publisher.trim();
+        var price = req.body.price && req.body.price.trim();
+        var description= req.body.description && req.body.description.trim();
+        var cover = req.body.cover && req.body.cover.trim();
+
+
+        Book.update({_id: req.params.id}, {
+            title:title,
+            category: category,
+            author: author,
+            publisher: publisher,
+            price: price,
+            description: description,
+            cover: cover
+
+        }, function(err) {
+            if(err) {
+                console.log('update error', err);
+            }
+
+            req.flash('success', "Book Updated");
+            res.location('/manage/books');
+            res.redirect('/manage/books');
+        });
+
+    });
+    
+    router.delete('/books/delete/:id', function(req, res){
+        Book.remove({_id: req.params.id}, function(err){
+            if (err) {
+                console.log(err);
+            }
+            
+            req.flash('success', "Book Deleted");
+            res.location('/manage/books');
+            res.redirect('/manage/books');
+        });
+        
+    });
+    
+    // Add a new category
+    router.post('/categories', function(req, res){
+        var name= req.body.name && req.body.name.trim();
+
+        if (name == '') {
+            req.flash('error', "Please fill out required fields");
+            res.location('/manage/categories/add');
+            res.redirect('/manage/categories/add');
+        }
+
+        var newCategory = new Category({
+            name:name
+        });
+
+        newCategory.save(function(err) {
+            if(err) {
+                console.log('save error', err);
+            }
+
+            req.flash('success', "Category Added");
+            res.location('/manage/categories');
+            res.redirect('/manage/categories');
+        });
+
+    });
+    
+    // Displays category edit form
+    router.get('/categories/edit/:id', function(req, res){
+        Category.findOne({_id:req.params.id}, function (err, category){
+            if (err) {
+                console.log(err);
+            }
+            var model ={
+                category: category
+            };
+            res.render('manage/categories/edit', model);
+        });
+    });
+    
+     // Edit category
+    router.post('/categories/edit/:id', function(req, res){
+        var name= req.body.name && req.body.name.trim();
+
+        Category.update({_id: req.params.id}, {
+            name:name
+        }, function(err) {
+            if(err) {
+                console.log('update error', err);
+            }
+
+            req.flash('success', "Category Updated");
+            res.location('/manage/categories');
+            res.redirect('/manage/categories');
+        });
+
+    });
+
+    // Delete category
+    router.delete('/categories/delete/:id', function (req, res) {
+        Category.remove({_id: req.params.id}, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            req.flash('success', "Category Deleted");
+            res.location('/manage/categories');
+            res.redirect('/manage/categories');
+        });
+    });
+    
+    
 };
