7d3cf04f1659027e37945e0c0110584a3561dbaf
marb <marb61a@gmail.com>
2015-06-05 08:11:34 +0000

    fixed route error and finished

--- controllers/books.js
+++ controllers/books.js
@@ -17,7 +17,7 @@ module.exports = function(router){
                 book : book
             };
             
-             res.render('books/details'); 
+             res.render('books/details', model); 
         });
                    
     });
